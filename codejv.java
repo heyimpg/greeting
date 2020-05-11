8831d0a8cfb8d95a7c0ee861155abd965a496007
ImPG <65141726+heyimpg@users.noreply.github.com>
2020-05-11 15:10:19 +0700

    Create codejv.java

---
+++ codejv.java
@@ -0,0 +1,9 @@
+package super_test;
+
+public class git_test {
+    public static void main(String[] args) {
+
+        System.out.println("Data java is here");
+        System.out.print("\n\n\n--------End Code--------");
+    }
+}
